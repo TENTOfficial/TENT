6fcc5c76cb5453d129b45354e47e8af2e32cec3b
SnowGem <ceo@snowgem.org>
2019-02-02 21:30:59 +0700

    fix getalldata from cli return error

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -24,6 +24,8 @@ public:
 static const CRPCConvertParam vRPCConvertParams[] =
 {
     { "getalldata", 0},
+    { "getalldata", 1},
+    { "getalldata", 2},
     { "stop", 0 },
     { "setmocktime", 0 },
     { "getaddednodeinfo", 0 },
