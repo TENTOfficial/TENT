b93967d10544792d7ae7c949df064e2df44581df
SnowGem <ceo@snowgem.org>
2019-04-22 23:37:41 +0700

    add get blockhash param

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -50,6 +50,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "getbalance", 1 },
     { "getbalance", 2 },
     { "getblockhash", 0 },
+    { "getblockhash", 1 },
     { "move", 2 },
     { "move", 3 },
     { "sendfrom", 2 },
