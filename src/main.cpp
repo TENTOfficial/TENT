834d3fa029ddc29cd0d11b00d98b39d431adc1f5
Snowgem <ceo@snowgem.org>
2019-05-20 23:44:20 +0700

    correct IsInitialBlockDownload

--- src/main.cpp
+++ src/main.cpp
@@ -2139,17 +2139,16 @@ bool IsInitialBlockDownload()
         return false;
 
     LOCK(cs_main);
-    if(chainActive.Height() != 0)
-    {
-        if (fImporting || fReindex)
-            return true;
-        if (chainActive.Tip() == NULL)
-            return true;
-        if (chainActive.Tip()->nChainWork < UintToArith256(chainParams.GetConsensus().nMinimumChainWork))
-            return true;
-        if (chainActive.Tip()->GetBlockTime() < (GetTime() - nMaxTipAge))
-            return true;
-    }
+
+    if (fImporting || fReindex)
+        return true;
+    if (chainActive.Tip() == NULL)
+        return true;
+    if (chainActive.Tip()->nChainWork < UintToArith256(chainParams.GetConsensus().nMinimumChainWork))
+        return true;
+    if (chainActive.Tip()->GetBlockTime() < (GetTime() - nMaxTipAge))
+        return true;
+
     LogPrintf("Leaving InitialBlockDownload (latching to false)\n");
     latchToFalse.store(true, std::memory_order_relaxed);
     return false;
