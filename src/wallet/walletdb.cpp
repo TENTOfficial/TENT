19af19900fffb9601d8057fe71f2c2ac42522fb0
Snowgem <ceo@snowgem.org>
2020-07-31 22:27:04 +0700

    compact wallet

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -1161,6 +1161,12 @@ bool BackupWallet(const CWallet& wallet, const string& strDest)
     return false;
 }
 
+bool CWalletDB::Compact(CDBEnv& dbenv, const std::string& strFile)
+{
+  bool fSuccess = dbenv.Compact(strFile);
+  return fSuccess;
+}
+
 //
 // Try to (very carefully!) recover wallet.dat if there is a problem.
 //
