69a4f749eac504a8998a185caf1a50d644d02adc
jrharp <numlock10@gmail.com>
2021-03-10 10:07:21 +1100

    Update

--- src/net.cpp
+++ src/net.cpp
@@ -122,6 +122,7 @@ static CNodeSignals g_signals;
 CNodeSignals& GetNodeSignals() { return g_signals; }
 
 // OpenSSL server and client contexts
+SSL_library_init();
 SSL_CTX *tls_ctx_server, *tls_ctx_client;
 
 static bool operator==(_NODE_ADDR a, _NODE_ADDR b)
