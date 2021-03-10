01897318e440bc4ac3c8f9f388e5f693c9f2cdb9
jrharp <numlock10@gmail.com>
2021-03-10 11:58:01 +1100

    update

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -27,7 +27,6 @@ $(package)_cxxflags_freebsd=-fPIC
 endef
 
 define $(package)_preprocess_cmds
-  echo "using $(boost_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
   patch -p1 < $($(package)_patch_dir)/darwin.diff
 endef
 
