9a56e475b80b023d5d6325ac3ab25e66c06eca9e
Snowgem <ceo@snowgem.org>
2021-04-09 22:34:26 -0700

    correct APPROX_RELEASE_HEIGHT

--- src/deprecation.h
+++ src/deprecation.h
@@ -10,7 +10,7 @@
 // * Shut down 100 weeks' worth of blocks after the estimated release block height.
 // * A warning is shown during the 2 weeks' worth of blocks prior to shut down.
 
-static const int APPROX_RELEASE_HEIGHT = 17170000;
+static const int APPROX_RELEASE_HEIGHT = 1717000;
 
 static const int WEEKS_UNTIL_DEPRECATION = 100;
 static const int DEPRECATION_HEIGHT = APPROX_RELEASE_HEIGHT + (WEEKS_UNTIL_DEPRECATION * 7 * 24 * 60);
